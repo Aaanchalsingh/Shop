36767e87a76556c03d7291ddc6ad991862e054a6
Aanchal Singh <aanchalsinghuk@gmail.com>
2024-03-17 22:08:16 +0530

    commit

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/account/register', authController.signup);
+router.post('/account/login', authController.login);
 
 module.exports = router;
